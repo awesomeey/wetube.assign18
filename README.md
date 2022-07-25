1c3cffbbfdba07e71d94fafbed73bb9a3ccd785d
awesomeey <awesomee2y@gmail.com>
2022-07-26 01:08:55 +0900

    wetube.assign18 #11.4 to #11.11

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # wetube.assign18
 Created with CodeSandbox
+
+# Video part Two
+* 오늘의 강의: 유튜브 클론코딩: From #11.4 to #11.11
+
+## Tasks
+Today we will finish making our video player! Based on today's videos implement the following controls for the video:
+
+* 현재 시간 표시
+* 타임라인(비디오 진행 상태, 클릭 시 점프)
+* 전체 화면
+* 단축키: Space를 눌러 일시 중지, 'F'를 눌러 전체 화면 모드로 들어가기, Esc 키를 눌러 전체 화면 모드에서 나오기
+
+****
+++ TASK 외 추가한 기능
+* default video controls hide + CSS styling
+* 컨트롤러 hide/show (3초 토글)   
+: pause 상태일 땐 show 유지
--- src/index.js
+++ src/index.js
@@ -1,50 +1,147 @@
+/*
+** TASK 외 추가한 기능
+** 1. default video controls hide + CSS styling
+** 2. 컨트롤러 hide/show (3초 토글) : pause 상태일 땐 show 유지
+** 
+*/
+
+const videoSection = document.getElementById("videoSection");
 const video = document.querySelector("video");
 const videoController = document.getElementById("videoController");
 const psBtn = videoController.querySelector("#playPauseBtn");
+const currentTime = videoController.querySelector("#currentTime");
+const totalTime = videoController.querySelector("#totalTime");
 const volumeBtn = videoController.querySelector("#volume");
 const volumeRange = videoController.querySelector("#volumeRange");
+const fullscreen = videoController.querySelector("#fullscreen");
 
+let controllerMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
 const handlePlayAndStop = () => {
-  if (video.paused) {
-    video.play();
-    psBtn.className = "fas fa-pause";
-  } else {
-    video.pause();
-    psBtn.className = "fas fa-play";
-  }
+	if (video.paused) {
+		video.play();
+		psBtn.className = "fas fa-pause";
+		controllerMovementTimeout = setTimeout(hideController, 3000);
+	} else {
+		video.pause();
+		psBtn.className = "fas fa-play";
+		initController();
+	}
 };
 
+const timeFormat = (time) =>
+	new Date(time * 1000).toISOString().substring(14, 19);
+
+const handleVideoTotalTime = () => {
+	const loadedTotalTime = Math.floor(video.duration);
+	totalTime.innerText = timeFormat(loadedTotalTime);
+	timeLine.max = loadedTotalTime;
+	videoController.classList.add("show");
+}
+
+const habndleTimeupdate = () => {
+	const loadedcurrentTime = Math.floor(video.currentTime);
+	currentTime.innerText = timeFormat(loadedcurrentTime);
+	timeLine.value = loadedcurrentTime;
+}
+
 const handleSound = () => {
-  if (video.muted) {
-    video.muted = false;
-    volumeRange.value = volumeValue;
-    volumeBtn.className = "fas fa-volume-up";
-  } else {
-    video.muted = true;
-    volumeRange.value = 0;
-    volumeBtn.className = "fas fa-volume-mute";
-  }
+	if (video.muted) {
+		video.muted = false;
+		volumeRange.value = volumeValue;
+		volumeBtn.className = "fas fa-volume-up";
+	} else {
+		video.muted = true;
+		volumeRange.value = 0;
+		volumeBtn.className = "fas fa-volume-mute";
+	}
 };
 
 const handleVolume = (event) => {
-  const {
-    target: { value }
-  } = event;
-  if (video.muted) {
-    video.muted = false;
-    volumeBtn.className = "fas fa-volume-mute";
-  }
-  if (value === "0") {
-    volumeBtn.className = "fas fa-volume-off";
-  } else {
-    volumeBtn.className = "fas fa-volume-up";
-  }
-  video.volume = volumeValue = value;
+	const {
+		target: { value }
+	} = event;
+	if (video.muted) {
+		video.muted = false;
+		volumeBtn.className = "fas fa-volume-mute";
+	}
+	if (value === "0") {
+		volumeBtn.className = "fas fa-volume-off";
+	} else {
+		volumeBtn.className = "fas fa-volume-up";
+	}
+	video.volume = volumeValue = value;
 };
 
+const handleTimeLine = (event) => {
+	const {
+		target: { value }
+	} = event;
+	video.currentTime = value;
+}
+
+const handleFullscreen = () => {
+	if(!document.fullscreenElement){
+		videoSection.requestFullscreen();
+		videoSection.classList.add("full");
+		fullscreen.className = "fas fa-solid fa-compress";
+	}else{
+		document.exitFullscreen();
+		videoSection.classList.remove("full");
+		fullscreen.className = "fas fa-solid fa-expand";
+	}
+}
+
+const handleKeyEvent = (event) => {
+  const { code } = event;
+	switch(code){
+		case "Space":
+			handlePlayAndStop();
+			break;
+		case "KeyF":
+			handleFullscreen();
+			break;
+		case "KeyM":
+			handleSound();
+			break;
+	}
+}
+
+const initController = () => {
+	if(controllerMovementTimeout){
+		clearTimeout(controllerMovementTimeout);
+		controllerMovementTimeout = null;
+	}
+	videoController.classList.add("show");
+}
+
+const hideController = () => videoController.classList.remove("show");
+
+const handleMouseMove = () => {
+	if(!video.paused){
+		if(controllerMovementTimeout) initController();
+		controllerMovementTimeout = setTimeout(hideController, 3000);
+	}
+}
+const handleMouseLeave = () => {
+	if(!video.paused)
+		controllerMovementTimeout = setTimeout(hideController, 3000);
+}
+
+video.readyState > 0
+	? handleVideoTotalTime()
+	: video.addEventListener("loadedmetadata", handleVideoTotalTime);
+
+video.addEventListener("timeupdate", habndleTimeupdate);
+video.addEventListener("click", handlePlayAndStop);
 psBtn.addEventListener("click", handlePlayAndStop);
 volumeBtn.addEventListener("click", handleSound);
 volumeRange.addEventListener("input", handleVolume);
+timeLine.addEventListener("input", handleTimeLine);
+fullscreen.addEventListener("click", handleFullscreen);
+
+videoSection.addEventListener("mousemove", handleMouseMove);
+videoSection.addEventListener("mouseleave", handleMouseLeave);
+window.addEventListener("keydown", handleKeyEvent);
