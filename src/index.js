26877d67386e63d8a6520ed033c10f74e4f9e345
awesomeey <64147732+awesomeey@users.noreply.github.com>
2022-07-26 01:24:56 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /*
-** TASK 외 추가한 기능
-** 1. default video controls hide + CSS styling
-** 2. 컨트롤러 hide/show (3초 토글) : pause 상태일 땐 show 유지
-** 3. keydown event 발생시마다 컨트롤러 show
-*/
+ ** TASK 외 추가한 기능
+ ** 1. default video controls hide + CSS styling
+ ** 2. 컨트롤러 hide/show (3초 토글) : pause 상태일 땐 show 유지
+ ** 3. keydown event 발생시마다 컨트롤러 show
+ */
 
 const videoSection = document.getElementById("videoSection");
 const video = document.querySelector("video");
@@ -21,121 +21,123 @@ let volumeValue = 0.5;
 video.volume = volumeValue;
 
 const handlePlayAndStop = () => {
-	if (video.paused) {
-		video.play();
-		psBtn.className = "fas fa-pause";
-		controllerMovementTimeout = setTimeout(hideController, 3000);
-	} else {
-		video.pause();
-		psBtn.className = "fas fa-play";
-		initController();
-	}
+  if (video.paused) {
+    video.play();
+    psBtn.className = "fas fa-pause";
+    controllerMovementTimeout = setTimeout(hideController, 3000);
+  } else {
+    video.pause();
+    psBtn.className = "fas fa-play";
+    initController();
+  }
 };
 
 const timeFormat = (time) =>
-	new Date(time * 1000).toISOString().substring(14, 19);
+  new Date(time * 1000).toISOString().substring(14, 19);
 
 const handleVideoTotalTime = () => {
-	const loadedTotalTime = Math.floor(video.duration);
-	totalTime.innerText = timeFormat(loadedTotalTime);
-	timeLine.max = loadedTotalTime;
-	videoController.classList.add("show");
-}
+  const loadedTotalTime = Math.floor(video.duration);
+  totalTime.innerText = timeFormat(loadedTotalTime);
+  timeLine.max = loadedTotalTime;
+  videoController.classList.add("show");
+};
 
 const habndleTimeupdate = () => {
-	const loadedcurrentTime = Math.floor(video.currentTime);
-	currentTime.innerText = timeFormat(loadedcurrentTime);
-	timeLine.value = loadedcurrentTime;
-}
+  const loadedcurrentTime = Math.floor(video.currentTime);
+  currentTime.innerText = timeFormat(loadedcurrentTime);
+  timeLine.value = loadedcurrentTime;
+};
 
 const handleSound = () => {
-	if (video.muted) {
-		video.muted = false;
-		volumeRange.value = volumeValue;
-		volumeBtn.className = "fas fa-volume-up";
-	} else {
-		video.muted = true;
-		volumeRange.value = 0;
-		volumeBtn.className = "fas fa-volume-mute";
-	};
-	handleMouseMove();
+  if (video.muted) {
+    video.muted = false;
+    volumeRange.value = volumeValue;
+    volumeBtn.className = "fas fa-volume-up";
+  } else {
+    video.muted = true;
+    volumeRange.value = 0;
+    volumeBtn.className = "fas fa-volume-mute";
+  }
+  handleMouseMove();
 };
 
 const handleVolume = (event) => {
-	const {
-		target: { value }
-	} = event;
-	if (video.muted) {
-		video.muted = false;
-		volumeBtn.className = "fas fa-volume-mute";
-	}
-	if (value === "0") {
-		volumeBtn.className = "fas fa-volume-off";
-	} else {
-		volumeBtn.className = "fas fa-volume-up";
-	}
-	video.volume = volumeValue = value;
+  const {
+    target: { value }
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    volumeBtn.className = "fas fa-volume-mute";
+  }
+  if (value === "0") {
+    volumeBtn.className = "fas fa-volume-off";
+  } else {
+    volumeBtn.className = "fas fa-volume-up";
+  }
+  video.volume = volumeValue = value;
 };
 
 const handleTimeLine = (event) => {
-	const {
-		target: { value }
-	} = event;
-	video.currentTime = value;
-}
+  const {
+    target: { value }
+  } = event;
+  video.currentTime = value;
+};
 
 const handleFullscreen = () => {
-	if(!document.fullscreenElement){
-		videoSection.requestFullscreen();
-		videoSection.classList.add("full");
-		fullscreen.className = "fas fa-solid fa-compress";
-	}else{
-		document.exitFullscreen();
-		videoSection.classList.remove("full");
-		fullscreen.className = "fas fa-solid fa-expand";
-	};
-	handleMouseMove();
-}
+  if (!document.fullscreenElement) {
+    videoSection.requestFullscreen();
+    videoSection.classList.add("full");
+    fullscreen.className = "fas fa-solid fa-compress";
+  } else {
+    document.exitFullscreen();
+    videoSection.classList.remove("full");
+    fullscreen.className = "fas fa-solid fa-expand";
+  }
+  handleMouseMove();
+};
 
 const handleKeyEvent = (event) => {
   const { code } = event;
-	switch(code){
-		case "Space":
-			handlePlayAndStop();
-			break;
-		case "KeyF":
-			handleFullscreen();
-			break;
-		case "KeyM":
-			handleSound();
-			break;
-	}
-}
+  switch (code) {
+    case "Space":
+      handlePlayAndStop();
+      break;
+    case "KeyF":
+      handleFullscreen();
+      break;
+    case "KeyM":
+      handleSound();
+      break;
+    default:
+      break;
+  }
+};
 
 const initController = () => {
-	if(controllerMovementTimeout){
-		clearTimeout(controllerMovementTimeout);
-		controllerMovementTimeout = null;
-	}
-	videoController.classList.add("show");
-}
+  if (controllerMovementTimeout) {
+    clearTimeout(controllerMovementTimeout);
+    controllerMovementTimeout = null;
+  }
+  videoController.classList.add("show");
+};
 
 const hideController = () => videoController.classList.remove("show");
 
 const handleMouseMove = () => {
-	if(!video.paused){
-		if(controllerMovementTimeout) initController();
-		controllerMovementTimeout = setTimeout(hideController, 3000);
-	}
-}
+  if (!video.paused) {
+    if (controllerMovementTimeout) initController();
+    controllerMovementTimeout = setTimeout(hideController, 3000);
+  }
+};
 const handleMouseLeave = () => {
-	if(!video.paused)
-		controllerMovementTimeout = setTimeout(hideController, 3000);
-}
+  if (!video.paused)
+    controllerMovementTimeout = setTimeout(hideController, 3000);
+};
 
 video.readyState > 0
-	? handleVideoTotalTime()
-	: video.addEventListener("loadedmetadata", handleVideoTotalTime);
+  ? handleVideoTotalTime()
+  : video.addEventListener("loadedmetadata", handleVideoTotalTime);
 
 video.addEventListener("timeupdate", habndleTimeupdate);
 video.addEventListener("click", handlePlayAndStop);
